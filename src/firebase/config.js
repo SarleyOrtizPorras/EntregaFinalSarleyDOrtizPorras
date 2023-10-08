8fe42048cc534443d506d699c6f79504c4fa777c
SarleyOrtizPorras <131881807+SarleyOrtizPorras@users.noreply.github.com>
2023-10-08 12:26:00 -0500

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAT3mDiDthOveYWq2dr_wIx6BtjuL5Xjfk",
+  authDomain: "ecommerce-94d90.firebaseapp.com",
+  projectId: "ecommerce-94d90",
+  storageBucket: "ecommerce-94d90.appspot.com",
+  messagingSenderId: "790990907702",
+  appId: "1:790990907702:web:a906c3c895b20fcdf1ab97"
+};
+
+const app = initializeApp(firebaseConfig);
+export const querydb = getFirestore(app)
